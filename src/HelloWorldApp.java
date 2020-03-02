dc94145ee99beca5b7bfff8d4f92104a7b1fd7ab
Alex.Dev <Alex.Dev@terieng.co.rs>
2020-03-02 11:52:53 +0100

    added new project

---
+++ src/HelloWorldApp.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author azigic
+ *
+ */
+public class HelloWorldApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello World");
+	}
+
+}
